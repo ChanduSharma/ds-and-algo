6eb631a79432202b1ac538422e974fe32be6992b
chandusharma <chandu.rikku@gmail.com>
2019-10-28 17:55:51 +0530

    Linked list implementations

--- karumanchi/Linked List/singly_ll.py
+++ karumanchi/Linked List/singly_ll.py
@@ -58,9 +58,36 @@ class LinkedList:
             current.set_next(new_node)
         self.length += 1
     
+    def insert_at_pos(self,data,pos):
+        if pos < 0 or pos > self.length:
+            return None
+        else:
+            if pos == 0:
+                self.insert_at_beg(data)
+            elif pos == self.length:
+                self.insert_at_end(data)
+            else:
+                new_node = Node()
+                new_node.set_data(data)
+                count = 1
+                current = self.head
+                while count < pos - 1:
+                    count += 1
+                    current = current.get_next()
+                new_node.set_next(current.get_next())
+                current.set_next(new_node)
+                self.length += 1
+    def del_at_beg(self):
+        if self.length == 0:
+            print("List is empty")
+        else:
+            temp = self.head
+            self.head = self.head.get_next()
+            self.length -= 1
+            del temp
+            
     def traverse(self):
         current = self.head
-        
         while current:
             print(current.get_data())
             current = current.get_next()
@@ -73,8 +100,14 @@ singly_linked_list.insert_at_beg(4)
 singly_linked_list.insert_at_beg(5)
 singly_linked_list.insert_at_beg(6)
 singly_linked_list.insert_at_end(8)
+singly_linked_list.insert_at_pos(82,0)
+singly_linked_list.insert_at_pos(83,2)
+singly_linked_list.insert_at_pos(84,0)
+singly_linked_list.insert_at_pos(85,4)
+singly_linked_list.insert_at_pos(86,9)
 
-singly_linked_list.traverse()   
-print(singly_linked_list.list_length())  
-            
+singly_linked_list.traverse()  
+singly_linked_list.del_at_beg()
+print("After deleting node")
+singly_linked_list.traverse()
     
