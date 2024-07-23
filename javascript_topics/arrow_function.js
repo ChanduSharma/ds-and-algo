62e020aac248202ad4706b05dfc56be35a855ca8
Chandra Sharma <chandu.rikku@gmail.com>
2024-07-23 22:33:06 +0530

    arrow function example

---
+++ javascript_topics/arrow_function.js
@@ -0,0 +1,5 @@
+const simpleArrowFunction = () => { return 3;}
+const funcWithargs = arg => console.log(`value of arg passed ${arg}.`);
+
+console.log("value from function simpleArrowFunction ",simpleArrowFunction());
+funcWithargs(3);
