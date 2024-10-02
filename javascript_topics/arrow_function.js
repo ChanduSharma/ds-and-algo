cc38bb573a991ee7be99848d81f7c06b03ddca28
Chandra Sharma <chandu.rikku@gmail.com>
2024-10-02 22:14:45 +0530

    examples hoisting and arrow func

--- javascript_topics/arrow_function.js
+++ javascript_topics/arrow_function.js
@@ -1,7 +1,7 @@
 const simpleArrowFunction = () => { return 3;}
 const funcWithargs = arg => console.log(`value of arg passed ${arg}.`);
 
-const funcwithmanyargs = (num1, num2) = num1 + num2);
+const funcwithmanyargs = (num1, num2) => num1 + num2;
 
 console.log(funcwithmanyargs(2,3));
 console.log("value from function simpleArrowFunction ",simpleArrowFunction());
